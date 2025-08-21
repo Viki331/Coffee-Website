4925f22ec92058ae7a4a6915602599d29f820a91
Viki331 <sahooviki131@gmail.com>
2025-08-21 20:46:54 +0530

    Commit2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks= document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton= document.querySelector("#menu-open-button");
 const menuCloseButton= document.querySelector("#menu-close-button");
 
@@ -6,3 +7,43 @@ menuOpenButton.addEventListener("click", ()=>{
 });
 
 menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());
+
+
+// Close menu when  the nav link is clicked
+navLinks.forEach(link=> {
+    link.addEventListener("click", ()=> menuCloseButton.click());
+})
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true, 
+  spaceBetween: 25,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768:{
+        slidesPerView: 2
+    },
+    1024:{
+        slidesPerView: 3
+    }
+  }
+
+});
